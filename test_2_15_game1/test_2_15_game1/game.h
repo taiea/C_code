8e674a3fe5e2d4ec494091472fa064b62ac6a1ba
taiea <isawfire@icloud.com>
2023-02-16 21:53:54 +0800

    删除

--- test_2_15_game1/test_2_15_game1/game.h
+++
@@ -1,8 +0,0 @@
-#define ROW 3
-#define COL 3
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-
-void Init_board(char board[ROW][COL], int row, int col);
-void Print_board(char board[ROW][COL], int row, int col);
-void Play_move(char board[ROW][COL], int row, int col);
